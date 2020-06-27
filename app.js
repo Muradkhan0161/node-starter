ec929676ce060821157fcc6a156b9081d2b535e7
Muradkhan0161 <murad.khan@northsouth.edu>
2020-06-27 10:32:54 +0600

    req routing

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const http = require('http');
 
 const server = http.createServer ((req, res) => {
-    console.log(req.url, req.method, req.headers)
-    //process.exit();
+    const url = req.url;
+    if(url === '/'){
+        res.write('<html>')
+        res.write('<head><title>This is the home</title></head>')
+        res.write('<body><form action="message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    res.setHeader('Content-Type', 'text.html')
+    res.write('<html>')
+    res.write('<head><title>Beginners with node.js</title></head>')
+    res.write('<body><h1>The reason i started with node.js</h1></body>')
+    res.write('</html>')
+    res.end()
 })
 
-server.listen(4000);
+server.listen(9000);
 
