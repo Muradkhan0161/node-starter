92ec56bd2c6ee2f79c6f0f59e22e2ab8f1389a35
Muradkhan0161 <murad.khan@northsouth.edu>
2020-06-28 00:34:50 +0600

    using node modules system and routes

--- app.js
+++ app.js
@@ -1,41 +1,10 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer ((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if(url === '/'){
-        res.write('<html>')
-        res.write('<head><title>This is the home</title></head>')
-        res.write('<body><form action="message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>')
-        res.write('</html>')
-        return res.end()
-    }
-    if(url === '/message' && method === 'POST') {
-        const body = [];
+const routes = require('./routes');
 
-        req.on('data', (chunk) => {
-            console.log(chunk)
-            body.push(chunk)
-        });
+console.log(routes.someText);
 
-        return req.on('end' , () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1]
-            fs.writeFile('message.txt' , message , err => {
-                res.statusCode = 302;
-                res.setHeader('location', '/');
-                return res.end();
-            })
-        })
-    }
-    res.setHeader('Content-Type', 'text.html')
-    res.write('<html>')
-    res.write('<head><title>Beginners with node.js</title></head>')
-    res.write('<body><h1>The reason i started with node.js</h1></body>')
-    res.write('</html>')
-    res.end()
-})
+const server = http.createServer(routes.handler);
 
 server.listen(9000);
 
