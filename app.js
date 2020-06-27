86380dd61d046314951af41732cfa190315add88
Muradkhan0161 <murad.khan@northsouth.edu>
2020-06-27 14:49:14 +0600

    redirecting request

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer ((req, res) => {
     const url = req.url;
+    const method = req.method;
     if(url === '/'){
         res.write('<html>')
         res.write('<head><title>This is the home</title></head>')
         res.write('<body><form action="message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>')
         res.write('</html>')
         return res.end()
+    }
+    if(url === '/message' && method === 'POST') {
+        res.statusCode = 302;
+        fs.writeFileSync('message.txt' , 'Mruad is here');
+        res.setHeader('location', '/');
+        return res.end();
     }
     res.setHeader('Content-Type', 'text.html')
     res.write('<html>')
